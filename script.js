89d608728e5c4ae21a45ffe59506e989e04e18a4
Brian Werner <brianmw199@gmail.com>
2023-05-04 22:38:55 -0500

    created a function called onReady to contain all of our color buttons and created a function to call .block to remove each color block individually

--- script.js
+++ script.js
@@ -1,1 +1,48 @@
-console.log('🟥 🟦 🟩 🟨');
+$(document).ready(onReady);
+// Pasted the <p> tags from html as a refrence to execute my code
+
+// {/* <p>Red Count: <span id="red-count">1</span></p> */}
+    // <p>Blue Count: <span id="blue-count">1</span></p>
+    // <p>Green Count: <span id="green-count">1</span></p>
+    // <p>Yellow Count: <span id="yellow-count">1</span></p>
+
+// created a function called onReady to contain all of my color buttons
+// and added a line to remove color blocks when clicked
+
+function onReady () {
+    $('#red-btn').on('click', appendRedContainer,); 
+    $('#blue-btn').on('click',appendBlueContainer);
+    $('#green-btn').on('click', appendGreenContainer);
+    $('#yellow-btn').on('click', appendYellowContainer);
+    $('#color-block').on('click', '.block', removeColorBlock);
+}
+
+// Created a set of different functions for each of my colors
+function appendRedContainer() {
+    $('#color-block').append('<div class="block red-fill"></div>');
+}
+function appendBlueContainer() {
+    $('#color-block').append('<div class="block blue-fill"></div>');
+}
+function appendGreenContainer() {
+    $('#color-block').append('<div class="block green-fill"></div>');
+}
+function appendYellowContainer() {
+    $('#color-block').append('<div class="block yellow-fill"></div>');
+}
+// Created a function to call .block to remove each block individually
+function removeColorBlock(){
+     $(this).remove();
+
+}
+
+
+
+
+
+
+
+
+
+
+    console.log('🟥 🟦 🟩 🟨');
